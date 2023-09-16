68901cdee56004c0d44ee14bf40048e5baa80ddb
Rohit <rohitfuleriya2001@gmail.com>
2023-09-16 17:31:21 +0530

    added Instgram Heart

--- add_frined/index.js
+++ add_frined/index.js
@@ -1,14 +1,21 @@
 var isStatus = document.querySelector("h5");
 
-var addFriend = document.querySelector('#add');
-var removeFriend = document.querySelector('#remove');
-
-addFriend.addEventListener("click", function(){
-    isStatus.innerHTML = "Friends added";
+var addFriend = document.querySelector("#add");
+// var removeFriend = document.querySelector('#remove');
+var check = 0;
+addFriend.addEventListener("click", function () {
+  if (check == 0) {
+    isStatus.innerHTML = "Angel Priya is added";
     isStatus.style.color = "green";
-});
-
-removeFriend.addEventListener("click", function() {
+    check = 1;
+  } else {
     isStatus.innerHTML = "Stranger";
     isStatus.style.color = "red";
+    check = 0;
+  }
 });
+
+// removeFriend.addEventListener("click", function() {
+//     isStatus.innerHTML = "Stranger";
+//     isStatus.style.color = "red";
+// });
